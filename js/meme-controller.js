76d47ff6a27559429ec7f56ac8c9e9909cfcdaa9
Jonathan Fridja <jonyfridja@gmail.com>
2019-06-06 10:25:42 +0300

    model, base ctor, onchange event

---
+++ js/meme-controller.js
@@ -0,0 +1,22 @@
+
+
+function init() {
+    console.log('loaded');
+    renderCanvas();
+}
+
+function onAddText() {
+    let memeTextEl = document.querySelector('#meme-text');
+    console.log(memeTextEl);
+    memeTextEl.focus();
+    addText()
+}
+
+
+function onChangeText() {
+    
+}
+
+function renderText() {
+    
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,20 @@
-console.log('service');
+let gMeme = {
+    txts: [
+        {
+            line: 'I am a meme',
+            size: 10,
+            align: 'center',
+            color: '#000',
+            pos: {x: 10, y: 10}
+        }
+    ]
+}
+
+function addText(line, size, align, color) {
+    let newText = createText(line, size, align, color);
+    gMeme.txts.push()
+}
+
+function createText(line, size, align, color) {
+    return { line, size, align, color };
+}
