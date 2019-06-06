a276bc996ff36ad2709ec3984a409e4bb8efa39b
Ben Pinchas <benpinchas@Stass-MacBook-Air.local>
2019-06-06 15:13:05 +0300

    texts editable

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,5 +1,9 @@
 let gCanvas;
 let gCtx;
+let gIsMouseDown = false //flag
+let gText = null
+let gMouseDownPos = null; //do nothing yet.
+
 function init() {
     gCanvas = document.querySelector('#meme-canvas');
     gCtx = gCanvas.getContext('2d');
@@ -12,7 +16,7 @@ function init() {
 function renderCanvas() {
     // render image
 
-
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
     // render texts
     let texts = getTexts();
     texts.forEach(text => {
@@ -22,44 +26,88 @@ function renderCanvas() {
 
         gCtx.font = `${text.size}px san-serif`;
         gCtx.fillStyle = text.color;
-        gCtx.fillText(text.line, textX , textY + text.outline.height);
+        gCtx.fillText(text.line, textX, textY + text.outline.height);
 
-        let outline = text.outline //mark 
-        let outlineHeight = outline.height;
-        let outlineWidth = outline.width;
-
-        gCtx.strokeRect(textX, textY, outlineWidth, outlineHeight)
         // gCtx.closePath();
     });
 
 }
 
 function onCanvasClick({ offsetX, offsetY }) {
-    console.log('canvas click!');
     console.log(offsetX, offsetY);
+    gIsMouseDown=true; //flag
+    gMouseDownPos = {x:offsetX, y:offsetY}
 
-    let text = getTexts().find(text => {
+    renderCanvas()
+
+    let text = getClickedText(offsetX, offsetY)
+    console.log('text',text);
+    if (text) {
+        renderOutline(text)
+        
+        gText = text
+    }
+}
+
+
+function onMouseMove({offsetX, offsetY}) {
+    if(!gIsMouseDown || !gText) return
+    // console.log('mouse move', gDraggedText);
+
+    let dX = gMouseDownPos.x - offsetX
+
+    console.log(gMouseDownPos.x, offsetX);
+    // gMouseDownPos.x 
+   
+    updatePos(gText, {x:offsetX - gText.outline.width/2, y:offsetY - gText.outline.height/2 })
+    renderCanvas()
+    renderOutline(gText)
+    // console.log(gDraggedText.pos);
+    
+}
+
+function onMouseUp() {
+    console.log('end');
+    gIsMouseDown=false;
+    gMouseDownPos = null;
+    gText = null;
+}
+
+
+function onEditText({offsetX, offsetY}) {
+    console.log('edit!');
+    let text = getClickedText(offsetX, offsetY);
+    if (text) gText = text;
+
+    let elEditInput =  document.querySelector('input#text-edit')
+    elEditInput.value = text.line
+    elEditInput.focus();
+}
+
+function onChangeText(el) {
+    updateText(gText, el.value)
+    renderCanvas()
+}
+
+function renderOutline(text) {
+    let outline = text.outline //mark 
+    let outlineHeight = outline.height;
+    let outlineWidth = outline.width;
+
+    gCtx.strokeRect(text.pos.x, text.pos.y, outlineWidth, outlineHeight)
+}
+
+
+
+function getClickedText(offsetX, offsetY) {
+    return getTexts().find(text => {
         return (offsetX > text.pos.x && offsetX < text.pos.x + text.outline.width)
             && (offsetY > text.pos.y && offsetY < text.pos.y + text.outline.height)
     })
-
-    console.log(text);
-
-    //check if in outline
 }
 
 
 
 
-function onAddText() {
-    let memeTextEl = document.querySelector('#meme-text');
-    console.log(memeTextEl);
-    memeTextEl.focus();
-    addText();
-}
 
 
-function onChangeText(textEl) {
-    updateText(textEl.value);
-    renderCanvas();
-}
