b7091db8994860828fc48ad7c328b976db7b6697
Ben Pinchas <benphs5@gmail.com>
2019-06-09 11:54:23 +0300

    flex order changing on mobile

--- js/img-service.js
+++ js/img-service.js
@@ -2,13 +2,12 @@ var gKeywords = { 'happy': 12, 'funny puk': 1 }
 
 var gImages = [
     {
-        id: 1, url: 'img/kids.jpg',
-        keywords: ['happy', 'dance', 'africa'
-    ]
-    },
-    {
         id: 1, url: 'img/kiss.jpg',
         keywords: ['gays', 'basketball', 'sport']
+    },
+    {
+        id: 1, url: 'img/dancing-women.jpg',
+        keywords: ['happy', 'dance', 'flowers', 'europe', 'field', 'nature']
     },
     {
         id: 1, url: 'img/dicaprio.jpg',
@@ -19,16 +18,16 @@ var gImages = [
         keywords: ['president', 'usa', 'america', 'barak obama']
     },
     {
+        id: 1, url: 'img/kids.jpg',
+        keywords: ['happy', 'dance', 'africa']
+    },
+    {
         id: 1, url: 'img/trump-red.jpg',
         keywords: ['president', 'donald trump', 'usa', 'america']
     },
     {
         id: 1, url: 'img/putin.jpg',
         keywords: ['president', 'russia', 'europe', 'putin']
-    },
-    {
-        id: 1, url: 'img/dancing-women.jpg',
-        keywords: ['happy', 'dance', 'flowers', 'europe', 'field', 'nature']
     },
     {
         id: 1, url: 'img/cat-laptop.jpg',
